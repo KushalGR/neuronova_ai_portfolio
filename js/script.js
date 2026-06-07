c33a5efab10f96c1826ea53825cf5b95c7db9b3a
KushalGR <136868345+KushalGR@users.noreply.github.com>
2026-06-07 11:50:04 +1000

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,64 @@
+
+//    NeuroNova AI Newsletter Form
+//    JavaScript Validation
+
+
+
+// Welcome Message 
+
+window.onload = function () {
+
+    alert(
+        "Welcome to the NeuroNova AI Ltd. - Sign up"
+    );
+
+};
+
+
+//  Form Validation 
+
+function validateForm() {
+
+    // Get First Name 
+
+    let firstName =
+        document.forms["signupForm"]["fname"].value;
+
+    // Get Last Name
+
+    let lastName =
+        document.forms["signupForm"]["lname"].value;
+
+    // Validate First Name 
+
+    if (firstName === "") {
+
+        alert(
+            "First Name is required."
+        );
+
+        return false;
+
+    }
+
+    // Validate Last Name 
+
+    if (lastName === "") {
+
+        alert(
+            "Last Name is required."
+        );
+
+        return false;
+
+    }
+
+    // Success Message 
+
+    alert(
+        "Thank you for signing up to the NeuroNova AI Newsletter!"
+    );
+
+    return true;
+
+}
